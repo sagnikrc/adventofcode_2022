73837230626b38cb619fb250e92b9d8a0b353a9f
Sagnik Roy Chowdhury <sagnik.roychowdhury@shift.com>
2022-12-03 19:00:06 -0800

    merge conflict

---
+++ day_2/day_2_part_1.py
@@ -0,0 +1,45 @@
+file1 = open('day_2_input.txt', 'r')
+Lines = file1.readlines()
+
+
+my_score = 0
+opponent_score = 0
+for line in Lines:
+    # if line.strip() == '':
+    my_pick = line[2]
+    opponent_pick = line[0]
+    round_score = 0 
+    if my_pick == 'X':
+        round_score += 1
+    elif my_pick == 'Y':
+        round_score += 2
+    else:
+        round_score += 3
+    
+    # opp rock
+    if opponent_pick == 'A' and my_pick == 'Y':
+        round_score += 6
+    elif opponent_pick == 'A' and my_pick == 'X':
+        round_score += 3
+    
+     # opp paper
+    elif opponent_pick == 'B' and my_pick == 'Z':
+        round_score += 6
+    elif opponent_pick == 'B' and my_pick == 'Y':
+        round_score += 3
+
+     # opp scissor
+    elif opponent_pick == 'C' and my_pick == 'X':
+        round_score += 6
+    elif opponent_pick == 'C' and my_pick == 'Z':
+        round_score += 3
+
+    else:
+        round_score += 0
+
+    my_score += round_score
+
+    print(f'opponent = {line[0]} | me = {line[2]} | myround_score = {round_score}' )
+
+
+print(f'final score = {my_score}')
---
+++ day_2/day_2_part_2.py
@@ -0,0 +1,52 @@
+file1 = open('day_2_input.txt', 'r')
+Lines = file1.readlines()
+
+my_score = 0
+opponent_score = 0
+for line in Lines:
+    # if line.strip() == '':
+    outcome = line[2]
+    opponent_pick = line[0]
+    round_score = 0 
+
+    # X means you need to lose, 
+    # Y means you need to end the round in a draw
+    # and Z means you need to win
+
+    if outcome == 'X':
+        round_score += 0
+    elif outcome == 'Y':
+        round_score += 3
+    else:
+        round_score += 6
+
+    # opp rock
+    if opponent_pick == 'A' and outcome == 'X': # choose scissor
+        round_score += 3
+    if opponent_pick == 'A' and outcome == 'Y': # choose rock
+        round_score += 1
+    if opponent_pick == 'A' and outcome == 'Z': # choose paper
+        round_score += 2
+    
+     # opp paper
+    if opponent_pick == 'B' and outcome == 'X': # choose rock
+        round_score += 1
+    if opponent_pick == 'B' and outcome == 'Y': # choose paper
+        round_score += 2
+    if opponent_pick == 'B' and outcome == 'Z': # choose scissor
+        round_score += 3
+
+     # opp scissor
+    if opponent_pick == 'C' and outcome == 'X': # choose paper
+        round_score += 2
+    if opponent_pick == 'C' and outcome == 'Y': # choose scissor
+        round_score += 3
+    if opponent_pick == 'C' and outcome == 'Z': # choose rock
+        round_score += 1
+
+    my_score += round_score
+
+    print(f'opponent = {line[0]} | outcome = {line[2]} | myround_score = {round_score}' )
+
+
+print(f'final score = {my_score}')
