700fb962ead6a1d765442c4b337da9233fd3dc93
Sagnik Roy Chowdhury <sagnik.roychowdhury@shift.com>
2022-12-03 19:42:14 -0800

    Day 3 part 2

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -22,3 +22,20 @@ for line in Lines:
     #print(f'{line} | first: {first_compartment} | second: {second_compartment} | common: {common}' )
 
 print(priority_sums)
+
+def score(c):
+    if 'a'<=c<='z':
+        return ord(c)-ord('a') + 1
+    else:
+        return ord(c)-ord('A') + 1 + 26
+
+p2 = 0
+X = [line for line in open('day_3_input.txt','r')]
+i = 0
+while i < len(X):
+    for c in X[i]:
+        if c in X[i+1] and c in X[i+2]:
+            p2 += score(c)
+            break
+    i += 3
+print(p2)
